2a5bd33c6b5a1859f0d62fc96de83ad495ac814f
CodeSadhu <patwardhanatharva6@gmail.com>
2023-04-06 11:01:07 +0530

    Removed UI/UX card from services

--- lib/utils/services_utils.dart
+++ lib/utils/services_utils.dart
@@ -3,7 +3,7 @@ import 'package:folio/utils/utils.dart';
 class ServicesUtils {
   static const List<String> servicesIcons = [
     StaticUtils.appDev,
-    StaticUtils.uiux,
+    // StaticUtils.uiux,
     StaticUtils.rapid,
     StaticUtils.blog,
     StaticUtils.openSource,
@@ -11,7 +11,7 @@ class ServicesUtils {
 
   static const List<String> servicesTitles = [
     "Mobile App Development",
-    "UI/UX Designing",
+    // "UI/UX Designing",
     "Rapid Prototyping",
     "Technical Blog Writing",
     "Open Source - GitHub",
@@ -19,8 +19,8 @@ class ServicesUtils {
 
   static const List<String> servicesDescription = [
     "Android app development via Flutter\n- Splash Screen\n- Firebase Auth/Cloud\n- REST APIs\n- Maps integration and more...!",
-    "Modern UI/UX Designing\n- Adobe XD\n- Mobile & Web designs\n- Interactive UI designs\n- Responsiveness\n- Promo Videos and more..!",
-    "Rapid Prototype via Flutter\n- Working MVP\n- Quick & Working prototype",
+    // "Modern UI/UX Designing\n- Adobe XD\n- Mobile & Web designs\n- Interactive UI designs\n- Responsiveness\n- Promo Videos and more..!",
+    "Rapid Prototype via Flutter\n- Working MVP\n- Quick & Working prototype in weeks, not months",
     "Technical Blog writing\n- Medium blogs\n- Soothing header images\n- SEO friendly\n- Researched topics and more..!",
     "Open source GitHub Projects\n- Awesome README.md\n- Well documented\n- Header images and more...!",
   ];
